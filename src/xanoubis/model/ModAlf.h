1a36e03f574a405999a7b488a8f883115cc37e56
Christian Hiesl <hiesl@genua.de>
2008-02-05 15:34:44 +0100

    200-100-150 GUI lokales Monitoring -- move modules to model
    Module classes belong to 'model' (not view), thus move them.
    The diff to all files but Makefile.am are empty.
    
    Aegis-Change: anoubis.10.650.C341 Delta 9
    Bug: 298
    BLE

---
+++ src/xanoubis/model/ModAlf.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2007 GeNUA mbH <info@genua.de>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __MODALF_H__
+#define __MODALF_H__
+
+#include "Module.h"
+
+enum ModAlfId {
+	MODALF_ID_BASE = 11000,
+	MODULE_ID_ENTRY(ALF,MAINPANEL),
+	MODULE_ID_ENTRY(ALF,OVERVIEWPANEL),
+	MODULE_ID_ENTRY(ALF,TOOLBAR)
+};
+
+class ModAlf : public Module
+{
+	public:
+		ModAlf(wxWindow *);
+		~ModAlf(void);
+
+		int	getBaseId(void);
+		int	getToolbarId(void);
+};
+
+#endif /* __MODALF_H__ */
