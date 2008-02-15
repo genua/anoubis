e9c2ded80b128a0832c11d84e6ded3752dd5d9f9
Christian Hiesl <hiesl@genua.de>
2008-02-15 11:06:19 +0100

    Client-Server Protocol - first library skeleton
    Add autotools sub-project for protocol library.
    
    Aegis-Change: anoubis.10.650.C413 Delta 40
    Bug: 348
    BLE

---
+++ src/libanoubisprotocol/server/apssetup.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2008 GeNUA mbH <info@genua.de>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdlib.h>
+#include <strings.h>
+
+#include "anoubisprotocol.h"
+#include "anoubisprotocolserver.h"
+
+struct ap_server *
+aps_create(void)
+{
+	struct ap_server *protocol;
+
+	protocol = (struct ap_server *)calloc(1, sizeof(struct ap_server));
+
+	return (protocol);
+}
+
+void
+aps_destroy(struct ap_server *protocol)
+{
+	bzero(protocol, sizeof(struct ap_server));
+	free((void *)protocol);
+}
