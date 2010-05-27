8acc4e624e31bd6ab2d4e493546d7d8e63edf051
Christian Hiesl <hiesl@genua.de>
2010-05-27 16:39:22 +0200

    Playground - add new skeleton cli
    Add new CLI for playground. This change just adds
    a skeleton without functionality.
    
    Aegis-Change: anoubis.10.972.C661 Delta 347
    Bug: 1497
    BLE

---
+++ src/playground/main.c
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2010 GeNUA mbH <info@genua.de>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "version.h"
+
+#ifdef S_SPLINT_S
+#include "splint-includes.h"
+#endif
+
+#include <sys/cdefs.h>
+#include <sys/param.h>
+#include <sys/types.h>
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "playground.h"
+
+extern char	*__progname;
+
+__dead void
+usage(void)
+{
+	fprintf(stderr, "usage: %s [-h] <command> [<program>]\n", __progname);
+	fprintf(stderr, "    <command>:\n");
+	fprintf(stderr, "	start <program>\n");
+
+	exit(1);
+}
+
+int
+main(int argc, char *argv[])
+{
+	int		 ch;
+	int		 error = 0;
+	char		*command = NULL;
+
+	if (argc < 2) {
+		usage();
+	}
+
+	/* Get command line arguments. */
+	while ((ch = getopt(argc, argv, "h")) != -1) {
+		switch (ch) {
+		case 'h':
+			usage();
+			break;
+		default:
+			usage();
+			/* NOTREACHED */
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	if (argc <= 0)
+		usage();
+		/* NOTREACHED */
+
+	command = *argv++;
+	argc--;
+
+	/* Run command. */
+	error = 0;
+	if (strcmp(command, "start") == 0) {
+		error = playground_start();
+	}
+
+	return (error);
+}
+
+static int
+playground_start(void)
+{
+	fprintf(stderr, "start: not yet implemented\n");
+	return (-ENOSYS);
+}
---
+++ src/playground/playground.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2010 GeNUA mbH <info@genua.de>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _PLAYGROUND_H_
+#define _PLAYGROUND_H_
+
+#include "config.h"
+
+#include <sys/cdefs.h>
+
+#ifdef LINUX
+#define __dead	__attribute__((__noreturn__))
+#endif
+
+/**
+ * Usage of program 'playground'.
+ * This will print out usage information to stderr and exit.
+ * The exit code is 1.
+ * @param None.
+ * @return Nothind.
+ */
+void usage(void) __dead;
+
+/**
+ * Start playground.
+ * This function is not yet implemented. This fact is also printed to stderr.
+ * @param None.
+ * @return -ENOSYS.
+ */
+static int playground_start(void);
+
+/**
+ * Anoubis playground CLI.
+ * This program starts new playgrounds.
+ * @param[in] 1st Number of arguments.
+ * @param[in] 2nd Arrya of strings with command line arguments.
+ * @return 0 on success.
+ */
+int main(int, char **);
+
+#endif	/* _PLAYGROUND_H_ */
